a8857cc87fb0c9ed0e6efd14147cb266eb46e531
Binalfew Kassa <binalfew@yahoo.com>
2025-06-15 18:20:13 +0300

    increase session duration

--- app/lib/session.server.ts
+++ app/lib/session.server.ts
@@ -6,7 +6,7 @@ export const authSessionStorage = createCookieSessionStorage({
     sameSite: "lax",
     path: "/",
     httpOnly: true,
-    maxAge: 60 * 10,
+    maxAge: 60 * 60 * 24 * 30, // 30 days
     secrets: process.env.SESSION_SECRET.split(","),
     secure: process.env.NODE_ENV === "production",
   },
